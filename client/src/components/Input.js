6e89b697ab4b7cc0869c35f0efa3a0d3f3639faf
root <gladiolus4231@gmail.com>
2021-05-11 07:55:05 +0000

    [Fix] #2 : add type, name, value, onChange props to Input Component
    Add value and onChange props in order to tract the input value.
    Add type and name to set respective property.
    Type prop is needed to set the password input type to password.

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const Input = ({icon, iconPosition, placeholder}) => {
+const Input = ({icon, iconPosition, placeholder, type, name, value, onChange}) => {
     return (
         <div className="field">
             <div className={`control ${iconPosition}`}>
-                <input className="input" type="text" placeholder={placeholder}/>
+                <input name={name} value={value} onChange={onChange} className="input" type={type} placeholder={placeholder}/>
                 {icon && 
                     <span className="icon is-small is-left">
                         <FontAwesomeIcon icon={icon} />

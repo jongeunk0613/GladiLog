7fc7521476aa9249251b8cccf427116cc6967966
root <gladiolus4231@gmail.com>
2021-05-12 04:37:12 +0000

    [Feat] #2 : use call procedure when creating user
    Instead of using sql directly, use the created stored procedure.
    Also, create a call_procedure method in the Databaseconnection class.
    It removes the repetition of typing db.open() and db.close().
    Using stored procedures is good for security, is faster, avoid repetition of codes, etc.

--- server/application/auth/views.py
+++ server/application/auth/views.py
@@ -2,18 +2,17 @@ from flask import request
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import auth
 from ..db.mysql_connection import DatabaseConnection
+import MySQLdb
 
 @auth.route('/signup', methods=['POST'])
 def signup():
     if request.method == 'POST':
         try: 
             db = DatabaseConnection()
-            db.open()
             data = request.get_json(force=True)
-            db.cursor.execute("INSERT INTO `TB_USER` (`email`, `username`, `password_hash`, `refresh_token`) VALUES (%s, %s, %s, %s)", [data.get('email'), data.get('username'), data.get('password'), "refreshtoken"])
-            db.conn.commit()
-            db.close()
+            result = db.call_procedure('CreateUser', (data.get('email'), data.get('username'), data.get('password'), "refreshtoken"), True);
         except (MySQLdb.Error, MySQLdb.Warning) as e:
             print(e)
+            raise e
         return "SIGNUP"
     
--- server/application/db/mysql_connection.py
+++ server/application/db/mysql_connection.py
@@ -17,17 +17,41 @@ class DatabaseConnection():
         self.cursor = None
 
     def open(self):
-        self.conn = MySQLdb.connect(
-            host=self.host, user=self.user, passwd=self.passwd,
-            db=self.db, charset=self.charset
-        )
-        self.cursor = self.conn.cursor()
+        try:
+            self.conn = MySQLdb.connect(
+                host=self.host, user=self.user, passwd=self.passwd,
+                db=self.db, charset=self.charset
+            )
+            self.cursor = self.conn.cursor()
+        except Exception as e:
+            raise e
 
     def close(self):
-        try: 
+        try:
             if self.cursor:
                 self.cursor.close()
                 if self.conn:
                     self.conn.close()
         except BaseException as e:
+            raise e
+
+    def call_procedure(self, procedure_name, params, with_commit=False):
+        try:
+            self.open()
+
+            if params is None:
+                self.cursor.callproc(procedure_name)
+            else:
+                self.cursor.callproc(procedure_name, params)
+
+            # result = [row for row in self.cursor]
+
+            if with_commit:
+                self.conn.commit()
+
+            self.close()
+
+            # return result
+        except Exception as e:
+            self.close()
             raise e

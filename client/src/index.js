897f1276dcf4f88651f034db65ae9fc1e3e5d463
root <gladiolus4231@gmail.com>
2021-05-12 08:03:50 +0000

    [Feat] #2 : set axios configuration
    In order to receive cookie from the server,
    cors settings must be done in axios.
    Since axios will be used multiple time throughout the entire project,
    set the settings in the index.js file.
    The important part is withCredentials=true.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import useAxios, { configure } from 'axios-hooks';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import 'bulma/css/bulma.min.css';
+
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    }
+})
+    
+configure({ instance });
 
 ReactDOM.render(
   <React.StrictMode>

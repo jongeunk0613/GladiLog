f55da84a9af27c6691ef0890f87542f5b14984c2
root <gladiolus4231@gmail.com>
2021-05-12 07:41:56 +0000

    [Settings] #2 : init jwt and cors
    JWT will be used for user authentication.
    In order to be able to store token cookies,
    cors configurations must be set.

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -1,11 +1,20 @@
 import time
-from flask import Flask, request
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from config import config
+
+jwt = JWTManager()
+cors = CORS()
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    jwt.init_app(app)
+    cors.init_app(app, origins=ALLOW_ORIGINS, supports_credentials=True)
 
     @app.route('/time')
     def get_current_time():

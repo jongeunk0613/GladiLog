9d5c2a7a564cb9063129a8c1bd77aec790b17ff9
root <gladiolus4231@gmail.com>
2021-05-12 05:51:45 +0000

    [Feat] #2 : update SignIn component with the changes
    Use useInputs hook to control multiple inputs with a single hook.
    Create a handleSubmit function.
    Add the necessary props to the input and form components.

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import AuthForm from '../components/AuthForm';
+import useInputs from '../utils/hooks/useInputs';
 
 const SignIn = () => {
+    const [state, onChange] = useInputs({
+        username: '',
+        password: ''
+    })
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+                
+        const response = await axios.post('/auth/signin', JSON.stringify(state));
+        
+        console.log(response.data)
+        
+        if (response.data.success === true){
+            
+            // history.push('/auth/signin')
+        }
+    }
+    
     return(
-        <AuthForm height="25%">
-            <Input icon={faUser} iconPosition="has-icons-left" placeholder="Username" />
-            <Input icon={faLock} iconPosition="has-icons-left" placeholder="Password" />
-            <Button color="is-info" content="SignIn" helpText="SignUp" /> 
+        <AuthForm height="25%" handleSubmit={handleSubmit}>
+            <Input icon={faUser} iconPosition="has-icons-left" placeholder="Username" type="text" name="username" value={state.username} onChange={onChange}/>
+            <Input icon={faLock} iconPosition="has-icons-left" placeholder="Password" type="password" name="password" onChange={onChange}/>
+            <Button color="is-info" content="SignIn" helpText="SignUp" />
         </AuthForm>
     )
 }

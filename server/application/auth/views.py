5ab2ad8c05ace9320587f3d825247b9ab1f08faf
root <gladiolus4231@gmail.com>
2021-05-12 08:18:04 +0000

    [Feat] #2 : create signin view
    First check whether the user has inserted the correct password.
    If so, create an access_token, set it on the cookie, and return.

--- server/application/auth/views.py
+++ server/application/auth/views.py
@@ -1,8 +1,9 @@
-from flask import request, jsonify
+from flask import request, jsonify, make_response, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, set_access_cookies
+import MySQLdb
 from . import auth
 from ..db.mysql_connection import DatabaseConnection
-import MySQLdb
 
 
 @auth.route('/signup', methods=['POST'])
@@ -11,9 +12,34 @@ def signup():
         try:
             db = DatabaseConnection()
             data = request.get_json(force=True)
-            db.call_procedure('CreateUser', (data.get('email'), data.get('username'), generate_password_hash(data.get('password'))), True);
+            db.call_procedure('CreateUser', [data.get('email'), data.get('username'), generate_password_hash(data.get('password'))], True)
         except (MySQLdb.Error, MySQLdb.Warning) as e:
             print(e)
             raise e
         return jsonify({'success': True})
 
+
+@auth.route('/signin', methods=['POST'])
+def signin():
+    if request.method == 'POST':
+        try:
+            db = DatabaseConnection()
+            data = request.get_json(force=True)
+            result = db.call_procedure('GetUser', [data.get('username')])[0]
+
+            if check_password_hash(result[3], data.get('password')):
+                access_token = create_access_token(identity=result[2])
+                resp = jsonify({'success': True})
+                set_access_cookies(resp, access_token)
+                return resp, 200
+            else:
+                return "BAD"
+
+            return "SIGNIN"
+        except Exception as e:
+            raise e
+
+
+@auth.route('/logout')
+def signout():
+    return "LOGOUT"

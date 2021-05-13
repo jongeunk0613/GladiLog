f2d261209255903ae7e7d849114c3468c9a8433d
root <gladiolus4231@gmail.com>
2021-05-13 04:34:37 +0000

    [Feat] #2 : logout, jwt_required, user_lookup_loader
    When logging out a user, simply use the unset_jwt_cookies function.
    By using jwt_required(), a route can be protected so that only
    authenticated users have access to it.
    When using jwt_required, it is necessary to define user_lookup_loader.
    It finds the user from the database based on the identity information
    saved in the jwt token.

--- server/application/auth/views.py
+++ server/application/auth/views.py
@@ -1,9 +1,18 @@
 from flask import request, jsonify, make_response, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import create_access_token, set_access_cookies
+from flask_jwt_extended import create_access_token, set_access_cookies, create_refresh_token, jwt_required, unset_jwt_cookies, current_user, get_jwt_identity
 import MySQLdb
 from . import auth
 from ..db.mysql_connection import DatabaseConnection
+from .. import jwt
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    db = DatabaseConnection()
+    user = db.call_procedure('GetUser', [identity])[0]
+    return user
 
 
 @auth.route('/signup', methods=['POST'])
@@ -14,7 +23,6 @@ def signup():
             data = request.get_json(force=True)
             db.call_procedure('CreateUser', [data.get('email'), data.get('username'), generate_password_hash(data.get('password'))], True)
         except (MySQLdb.Error, MySQLdb.Warning) as e:
-            print(e)
             raise e
         return jsonify({'success': True})
 
@@ -25,10 +33,11 @@ def signin():
         try:
             db = DatabaseConnection()
             data = request.get_json(force=True)
-            result = db.call_procedure('GetUser', [data.get('username')])[0]
+            user = db.call_procedure('GetUser', [data.get('username')])[0]
 
-            if check_password_hash(result[3], data.get('password')):
-                access_token = create_access_token(identity=result[2])
+            if check_password_hash(user['password_hash'], data.get('password')):
+                access_token = create_access_token(identity=user['username'])
+                refresh_token = create_refresh_token(identity=user)
                 resp = jsonify({'success': True})
                 set_access_cookies(resp, access_token)
                 return resp, 200
@@ -42,4 +51,12 @@ def signin():
 
 @auth.route('/logout')
 def signout():
-    return "LOGOUT"
+    resp = jsonify({'success': True})
+    unset_jwt_cookies(resp)
+    return resp
+
+
+@auth.route('/protected')
+@jwt_required()
+def protected():
+    return jsonify({'success': True}), 200

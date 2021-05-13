92e9dd4e13905d0d6dc923abafc8cba5889e2115
root <gladiolus4231@gmail.com>
2021-05-13 09:23:54 +0000

    [Feat] #2 : change message component styling
    When a message is shown, the authentication form moves sligthly.
    To prevent this, set the height to 100% and set a default text.

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -9,7 +9,8 @@ const MessagePosition = styled.div`
 
 const MessageContainer = styled.div`
     width: 40%;
-    margin: 1.5rem;
+    height: 100%;
+    padding: 1.5rem;
 `;
 
 const Message = ({content, type, show}) => {

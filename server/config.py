9f4f4f9715681d9a81a7873defadd71156153155
root <gladiolus4231@gmail.com>
2021-05-13 04:26:32 +0000

    [Setting] #2 : set JWT_COOKIE_SECURE and TOKEN_EXPIRES
    JWT_COOKIE_SECURE is set to allow cookies to be sent only in https connections.
    However, since in development environment the connections are in http, it is set to false.
    Also set the access token expiration time to 30 minutes.

--- server/config.py
+++ server/config.py
@@ -1,14 +1,18 @@
 import os
+import datetime
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'hard to guess string'
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY') or 'hard to guess string 2'
+    
     JWT_TOKEN_LOCATION = [os.getenv('JWT_TOKEN_LOCATION') or 'cookies']
-    JWT_COOKIE_SECURE = os.getenv('JWT_COOKIE_SECURE') or False # true on production
+    JWT_COOKIE_SECURE = os.getenv('JWT_COOKIE_SECURE').lower() in ('true', '1', 't')# true on production
     JWT_ACCESS_COOKIE_PATH = os.getenv('JWT_ACCESS_COOKIE_PATH') or '/'
     JWT_COOKIE_CSRF_PROTECT = True
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=30)
+    
     MYSQL_HOST = os.getenv('DB_HOST')
     MYSQL_USER = os.getenv('DB_USERNAME')
     MYSQL_PASSWORD = os.getenv('DB_PASSWORD')

2b3ab6a1e248fa812d8be7dd1158dcb8f3bac755
root <gladiolus4231@gmail.com>
2021-05-13 09:29:15 +0000

    [Feat] #2 : show server error message to the user
    Use the created Message component to show error messages to the user.
    Control the states related to the message using useState.

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { faEnvelope, faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import Input from '../components/Input';
 import Button from '../components/Button';
@@ -14,13 +14,18 @@ const SignUp = ({history}) => {
         username: '',
         password: ''
     })
-    
     const [{data}, refetch] = useAxios({url: '/auth/signup', method: 'POST'}, {manual: true});
+    const [message, setMessage] = useState({content: 'something', type: '', show: false});
     
     const handleSubmit = async (event) => {
         event.preventDefault();
-                
-        await refetch({data: JSON.stringify(state)});
+        
+        setMessage({content: 'Waiting for the server to respond...', type: 'is-warning', show: true});
+        try {
+            await refetch({data: JSON.stringify(state)});
+        } catch(e) {
+            setMessage({content: e.response.data.msg, type: 'is-danger', show: true});
+        }
                         
         if (data && data.success === 200){
             console.log("200")
@@ -30,7 +35,7 @@ const SignUp = ({history}) => {
     
     return(
         <>
-            <Message content="SOMETHING" type="is-danger" show={false}/>
+            <Message content={message.content} type={message.type} show={message.show}/>
             <AuthForm height="45%" handleSubmit={handleSubmit}>
                 <Input icon={faEnvelope} iconPosition="has-icons-left" placeholder="Email" type="text" name="email" value={state.email} onChange={onChange}/>
                 <Input icon={faUser} iconPosition="has-icons-left" placeholder="Username" type="text" name="username" value={state.username} onChange={onChange}/>

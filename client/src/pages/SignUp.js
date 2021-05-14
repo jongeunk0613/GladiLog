8ef3f6034a23d9d0f3a736a564f34ff36d421f68
root <gladiolus4231@gmail.com>
2021-05-14 04:13:59 +0000

    [Feat] #2 : use axios and redirect
    Intead os useAxios use axios.
    Using the data variable from useAxios updates the component constantly.
    
    When the account creation is successful, set the message to the user
    and redirect the user to the login page after 1.5 seconds.

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { faEnvelope, faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
+
 import Input from '../components/Input';
 import Button from '../components/Button';
 import AuthForm from '../components/AuthForm';
 import useInputs from '../utils/hooks/useInputs';
-import useAxios from 'axios-hooks';
 import Message from '../components/Message';
 
 
@@ -14,7 +15,6 @@ const SignUp = ({history}) => {
         username: '',
         password: ''
     })
-    const [{data}, refetch] = useAxios({url: '/auth/signup', method: 'POST'}, {manual: true});
     const [message, setMessage] = useState({content: 'something', type: '', show: false});
     
     const handleSubmit = async (event) => {
@@ -22,14 +22,16 @@ const SignUp = ({history}) => {
         
         setMessage({content: 'Waiting for the server to respond...', type: 'is-warning', show: true});
         try {
-            await refetch({data: JSON.stringify(state)});
+            const response = await axios.post('/auth/signup', JSON.stringify(state))
+            if (response.status === 200) {
+                setMessage({content: response.data.msg, type: 'is-success', show: true});
+                setTimeout(() => {
+                    history.push('/auth/signin');
+                }, 1500);
+            }
+            
         } catch(e) {
             setMessage({content: e.response.data.msg, type: 'is-danger', show: true});
-        }
-                        
-        if (data && data.success === 200){
-            console.log("200")
-            // history.push('/auth/signin')
         }
     }
     

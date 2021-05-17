e3f356973742b5cd7d46b23b97e827faeedf01a9
root <gladiolus4231@gmail.com>
2021-05-17 04:18:56 +0000

    [Refactor] : set padding in base component
    Instead of setting padding property to every component in the body,
    set the padding property in the base component.

--- client/src/components/Base.js
+++ client/src/components/Base.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import NavBar from './NavBar';
+import styled from 'styled-components';
+
+const BodyContainer = styled.div`
+    padding: 2rem;
+`;
 
 const Base = ({children}) => {
     return(
         <>
             <NavBar/>
-            {children}
+            <BodyContainer>
+                {children}
+            </BodyContainer>
         </>
     )
 }

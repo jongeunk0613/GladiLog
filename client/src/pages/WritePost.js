df110c17897dc3546edc4281df07b9c5a0c037d6
root <gladiolus4231@gmail.com>
2021-05-17 05:25:52 +0000

    [Feat] #5 : use the created PostForm component
    use the created post form component when writing a new form.

--- client/src/pages/WritePost.js
+++ client/src/pages/WritePost.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PostForm from '../components/PostForm';
 
 const WritePost = () => {
     return (
-        <div>WRITE POST</div>
+        <PostForm title="제목을 입력하세요" submitButtonName="완료"/>
     )
 }
 

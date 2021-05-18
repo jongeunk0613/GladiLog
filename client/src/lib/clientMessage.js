6e2f2fc66f392d7114ebcb7418670d5a8faa562e
root <gladiolus4231@gmail.com>
2021-05-18 04:32:54 +0000

    [Feat] #5 : change notEmpty message
    Explicitly indicate that no whitespace is allowed.

--- client/src/lib/clientMessage.js
+++ client/src/lib/clientMessage.js
@@ -1,6 +1,6 @@
-export default clientMessage = {
+export default {
     email: "Incorrect email format.",
     password: "Letters, numbers, special characters. Longer than 8.",
     password2: "The password does not match.",
-    notEmpty: "Cannot be empty",
+    notEmpty: "Cannot be empty or whitespace.",
 }

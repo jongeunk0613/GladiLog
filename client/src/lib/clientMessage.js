a8a24f2887fffc5f00869264075ed3686250083a
root <gladiolus4231@gmail.com>
2021-05-18 02:30:26 +0000

    [Feat] #5 : group client messages
    Group and organize client messages in one file.

---
+++ client/src/lib/clientMessage.js
@@ -0,0 +1,6 @@
+export default clientMessage = {
+    email: "Incorrect email format.",
+    password: "Letters, numbers, special characters. Longer than 8.",
+    password2: "The password does not match.",
+    notEmpty: "Cannot be empty",
+}

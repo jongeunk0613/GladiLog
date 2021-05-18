b9adb722aa6abb226cb906698aea2fc795956e66
root <gladiolus4231@gmail.com>
2021-05-18 04:41:58 +0000

    [Feat] #5 : use validators in signup and signin form
    Manage the validation state using useState.
    Check validation status using the created validators.
    For this, create a new onInput function that checks
    if the field values are validated and updates them in the state
    every time the user changes the value of the input.
    Before submitting the form, check the validation status.
    If one of them is false, show the message to the user.

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import { useSelector, useDispatch } from 'react-redux';
 import { set } from '../modules/message';
@@ -9,18 +9,48 @@ import Button from '../components/Button';
 import AuthForm from '../components/AuthForm';
 import useInputs from '../hooks/useInputs';
 import Message from '../components/Message';
+import clientMessage from '../lib/clientMessage';
+import { isNotEmpty } from '../lib/validator';
 
 const SignIn = ({history}) => {
     const [state, onChange] = useInputs({
         username: '',
         password: ''
     });
+    const [isValid, onValidation] = useState({
+        username: false,
+        password: false,
+    })
     const {content, type, show} = useSelector(state => state.message);
     const dispatch = useDispatch();
     const setMessage = useCallback((content, type, show) => dispatch(set(content, type, show)), [dispatch]);
     
+    const onInput = (e) => {
+        switch(e.target.name){
+            case 'username':
+                onValidation({
+                    ...state,
+                    username: isNotEmpty(e.target.value)
+                });
+                break;
+            case 'password':
+                onValidation({
+                    ...state,
+                    password: isNotEmpty(e.target.value)
+                })
+                break;
+            default:
+        }
+        onChange(e);
+    }
+    
     const handleSubmit = async (event) => {
         event.preventDefault();
+        
+        if (!state.username || !state.password){
+            setMessage('The form is not correcty filled out.', 'is-warning', true);
+            return;
+        } 
         
         setMessage('Waiting for the server to respond...', 'is-warning', true);
         try {
@@ -38,9 +68,9 @@ const SignIn = ({history}) => {
     return(
         <>
             <Message content={content} type={type} show={show}/>
-            <AuthForm height="30%" handleSubmit={handleSubmit}>
-                <Input icon={faUser} iconPosition="has-icons-left" placeholder="Username" type="text" name="username" value={state.username} onChange={onChange}/>
-                <Input icon={faLock} iconPosition="has-icons-left" placeholder="Password" type="password" name="password" onChange={onChange}/>
+            <AuthForm height="16rem" handleSubmit={handleSubmit}>
+                <Input icon={faUser} iconPosition="has-icons-left" placeholder="Username" type="text" name="username" value={state.username} isValid={isValid.username} onChange={onInput} validationMessage={clientMessage.notEmpty}/>
+                <Input icon={faLock} iconPosition="has-icons-left" placeholder="Password" type="password" name="password" value={state.password} isValid={isValid.password} onChange={onInput} validationMessage={clientMessage.notEmpty}/>
                 <Button color="is-info" content="SignIn" helpText="SignUp" />
             </AuthForm>
         </>

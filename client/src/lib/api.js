33ae86d5ed8b04c9ebbed3c127c4f4e8f15fbdf2
root <gladiolus4231@gmail.com>
2021-05-18 07:33:31 +0000

    [Feat] #6 : create api for get posts
    Create axios call for getting all posts.

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -4,3 +4,4 @@ axios.defaults.withCredentials = true
 export const signup = async (data) => await axios.post('/auth/signup', data);
 export const signin = async (data) => await axios.post('/auth/signin', data);
 export const writePost = async (data) => await axios.post('/post/write', data);
+export const getPosts = async () => await axios.get('/post/');

ca1c61407087c9648bb14a952f2df941ec9d1edc
root <gladiolus4231@gmail.com>
2021-05-18 06:28:49 +0000

    [Feat] #6 : Post component
    Receives a post object containing all the information of a single post.
    Show the title, date, and the content.

---
+++ client/src/components/Post.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 14rem;
+    padding: 1rem;
+    margin-top: 1rem;
+`;
+
+const Top = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Title = styled.div`
+    font-weight: bold;
+    font-size: 1.5rem;
+`;
+
+const Date = styled.div`
+
+`;
+
+const Body = styled.div`
+    height: 85%;
+    text-align: justify;
+    word-wrap: break-word;
+    overflow: hidden;
+`;
+
+const Post = ({post}) => {
+    console.log(post.body.replace(/\s\s+/g, ' '))
+    return (
+        <Container>
+            <Top>
+                <Title>{post.title}</Title>
+                <Date>{post.created}</Date>
+            </Top>
+            <Body>
+                {post.body.replace(/\s\s+/g, ' ')}
+            </Body>
+            <hr/>
+        </Container>
+    );
+}
+
+export default Post;

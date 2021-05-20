e394caa61b1d5a343939499fade0f368dacf47e1
root <gladiolus4231@gmail.com>
2021-05-20 04:40:01 +0000

    [Feat] #8 : write api call function for getPost
    Create api call function for getting a post with the given id.

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -5,3 +5,4 @@ export const signup = async (data) => await axios.post('/auth/signup', data);
 export const signin = async (data) => await axios.post('/auth/signin', data);
 export const writePost = async (data) => await axios.post('/post/write', data);
 export const getPosts = async () => await axios.get('/post/');
+export const getPost = async (id) => await axios.get(`/post/${id}`);

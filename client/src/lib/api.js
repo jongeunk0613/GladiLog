03a2305f7f7236c21a27b750da2910096a9f091a
root <gladiolus4231@gmail.com>
2021-05-20 05:35:52 +0000

    [Feat] #8 : create delete post api
    Create the api call function to delete a post with the given id.

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -6,3 +6,4 @@ export const signin = async (data) => await axios.post('/auth/signin', data);
 export const writePost = async (data) => await axios.post('/post/write', data);
 export const getPosts = async () => await axios.get('/post/');
 export const getPost = async (id) => await axios.get(`/post/${id}`);
+export const deletePost = async (id) => await axios.delete(`/post/delete/${id}`);

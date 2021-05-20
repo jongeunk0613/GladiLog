e50c6dd2d8b3a8541035fec0168348d0f45bbfd0
root <gladiolus4231@gmail.com>
2021-05-20 04:46:00 +0000

    [Feat] #8 : Post component
    It shows all the detailed information of a single post.

---
+++ client/src/components/Post.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PostContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    border-bottom: 1px solid #CDCDCD;
+    padding-bottom: 0.5rem;
+`;
+
+const Title = styled.div`
+    font-size: 1.5rem;
+    font-weight: bold;
+`;
+
+const VerticalLine = styled.div`
+    border-right: 1px solid grey;
+`;
+
+const Info = styled.div`
+    display: flex;
+    font-size: 1rem;
+    color: grey;
+
+    & > div {
+        margin: 0.25rem;
+    }
+
+    & > div:nth-child(n+5), div:nth-child(n+7){
+        cursor: pointer;
+    }
+`;
+
+const Body = styled.div`
+    padding-top: 1rem;
+    text-align: left;
+`;
+
+const Post = ({post}) => {
+    console.log(post);
+    
+    return (
+        <PostContainer>
+            <Header>
+                <Title>{post.title}</Title>
+                <Info>
+                    <div>{post.username}</div><VerticalLine/>
+                    <div>{post.created}</div><VerticalLine/>
+                    <div className="post_button">수정</div><VerticalLine/>
+                    <div className="pots_button">삭제</div>
+                </Info>
+            </Header>
+            <Body>
+                 {post.body}
+            </Body>
+        </PostContainer>
+    );
+}
+
+export default Post;

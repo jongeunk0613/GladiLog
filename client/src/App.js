c25be2eb8d4c6a118643e1f45bd848bf7d8e146b
root <gladiolus4231@gmail.com>
2021-05-20 01:40:18 +0000

    [Feat] : make route match exactly one component
    The exact property of the component is useful when it is inside the Switch component.
    Since post/write also matched in /post/:id, the two components where shown.
    To prevent this, Route components where wrapped by Switch.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Main from './pages/Main';
@@ -13,11 +13,13 @@ function App() {
   return (
     <div className="App">
         <Base>
-            <Route path="/" exact component={Main} />
-            <Route path="/auth/signup" exact component={SignUp} />
-            <Route path="/auth/signin" exact component={SignIn} />
-            <Route path="/post/write" exact component={WritePost} />
-            <Route path="/post/:id" exact component={Post} />
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/auth/signup" exact component={SignUp} />
+                <Route path="/auth/signin" exact component={SignIn} />
+                <Route path="/post/write" exact component={WritePost} />
+                <Route path="/post/:id" exact component={Post} />
+            </Switch>
         </Base>
     </div>
   );

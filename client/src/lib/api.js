794eab20ada0122518e0e7bfc0028d912d47f2d7
root <gladiolus4231@gmail.com>
2021-05-20 06:57:50 +0000

    [Feat] #8 : update post api
    Create api function that calls to update post api.

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -7,3 +7,4 @@ export const writePost = async (data) => await axios.post('/post/write', data);
 export const getPosts = async () => await axios.get('/post/');
 export const getPost = async (id) => await axios.get(`/post/${id}`);
 export const deletePost = async (id) => await axios.delete(`/post/delete/${id}`);
+export const updatePost = async (id, data) => await axios.patch(`/post/edit/${id}`, data);

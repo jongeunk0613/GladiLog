a832e3230bf317ade23333966a1a39d020a6eff4
root <gladiolus4231@gmail.com>
2021-05-21 06:36:53 +0000

    [Feat] #10 : pass url to Modal from Auth component
    Instead of creating an onClick function everytime the Modal component is used,
    simply pass the url where the user will be redirected.
    The redirection is done inside the modal.

--- client/src/lib/Auth.js
+++ client/src/lib/Auth.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Modal from '../components/Modal';
 
 const Auth = ({PageComponent}) => {
     const { username } = useSelector(state => state.user);
-    const history = useHistory();
-    
-    const onClick = () => {
-        history.push('/auth/signin');
-    }
     
     if (!username){
-        return <Modal contentTitle="Signin required." contentBody={`You must sign in to perform the requested action. ${"\n"} Will be redirected to signin page.`} onClick={onClick}/>
+        return <Modal contentTitle="Signin required." contentBody={`You must sign in to perform the requested action. ${"\n"} Will be redirected to signin page.`} url="/auth/signin"/>
     }
     
     return (

53df3b604af725d2b9bf09d96be41b7d819ca80a
root <gladiolus4231@gmail.com>
2021-05-21 06:35:41 +0000

    [Feat] #10 : check user before editing
    If the user trying to edit the post is not the author of the post,
    redirect to the previous page using history hook.

--- client/src/pages/PostEdit.js
+++ client/src/pages/PostEdit.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import * as api from '../lib/api';
 import PostForm from '../components/PostForm';
+import Modal from '../components/Modal';
 
 const PostEdit = () => {
     const { id } = useParams();
     const history = useHistory();
+    const { username } = useSelector(state => state.user);
+    const [modal, setModal] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [state, setState] = useState({
@@ -20,6 +24,9 @@ const PostEdit = () => {
             try{
                 const resolved = await api.getPost(id);
                 setState(resolved.data.data);
+                if (resolved.data.data.username !== username){
+                    setModal(true);
+                }
             } catch (e) {
                 setError(e);
             }
@@ -64,6 +71,7 @@ const PostEdit = () => {
     
     return (
         <>
+            {modal && <Modal contentTitle="Not authorized" contentBody="You cannot edit someone else's post."/>}
             {!loading && <PostForm title="Post Title" value={state.title} body={state.body} submitButtonName="Save" onChange={onChange} handleCancel={handleCancel} handleSubmit={handleSubmit}/>}
         </>
     )

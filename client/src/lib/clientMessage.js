bd5a930b08f5ac7222dbffc7eeb8c2272bf8099d
root <gladiolus4231@gmail.com>
2021-05-24 02:12:43 +0000

    [Refactor] : give name to clientMessag object
    Export default must contain in a variable.
    Since error was showing, did as indicated.

--- client/src/lib/clientMessage.js
+++ client/src/lib/clientMessage.js
@@ -1,6 +1,8 @@
-export default {
+const clientMessage = {
     email: "Incorrect email format.",
     password: "Letters, numbers, special characters. Longer than 8.",
     password2: "The password does not match.",
     notEmpty: "Cannot be empty or whitespace.",
 }
+
+export default clientMessage;

a561ed5989d286c9fdbbbca8e2fe07c0ae0f5c41
root <gladiolus4231@gmail.com>
2021-05-24 02:02:19 +0000

    [Feat] #12 : style button
    Instead of using bulma button, a personally styled button component was created.
    This is because bulma's button was hard to properly place for unknown
    css placement properties.
    When using two buttons, and wanting to center them,
    only the first button was centered and the second button not.
    Therefore, a new button was created.

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const type = {
+    "is-info": {
+        "background": "#3e8ed0",
+        "color": "#fff"
+    },
+    "is-success": {
+        "background": "#48c78e",
+        "color": "#fff"
+    },
+    "is-white": {
+        "background": "#fff",
+        "color": "##0a0a0a"
+    }
+}
+
+const ButtonContainer = styled.div`
+    
+`;
+
+const StyledButton = styled.button`
+    background-color: ${props => type[props.type]["background"]};
+    color: ${props => type[props.type]["color"]};
+    border-color: transparent;
+    border-radius: 0.5rem;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    margin: 0.5rem;
+`
 
 const Button = ({color, content, helpText, onClick}) => {
     return (
-        <div className="field">
-            <p className="control">
-                <button className={`button ${color}`} style={{marginTop: "0.5rem"}} onClick={onClick}>
-                    {content}
-                </button>
-            </p>
+        <ButtonContainer>
+            <StyledButton onClick={onClick} type={color}>
+                {content}
+            </StyledButton>
             {helpText && <Link to={`${helpText.toLowerCase()}`} className="help">{helpText}</Link>}
-        </div>
+        </ButtonContainer>
     )
 }
 

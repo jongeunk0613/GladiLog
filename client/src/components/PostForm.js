4eab8d290332e03f62236d899f37898be11b65b0
root <gladiolus4231@gmail.com>
2021-05-24 02:08:23 +0000

    [Feat] #12 : fix style of buttons in PostWrite
    Center, space between, margin-right the buttons

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -32,16 +32,21 @@ const Footer = styled.footer`
     background: #f2f2f2;
     width: 100vw;
     heigth: 15vh;
-
-    div {
-        padding-top: auto;
-        padding-bottom: auto;
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: flex-start;
-        margin-right: 1.5rem;
-    }
 `
+
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+
+    div{
+        display: flex;
+        align-items: center;
+    }
+
+    div:nth-child(1) {
+        margin-right: 1rem;
+    }
+`;
 
 const PostForm = ({title, value, body, submitButtonName, onChange, handleSubmit, handleCancel}) => {
     return (
@@ -49,10 +54,10 @@ const PostForm = ({title, value, body, submitButtonName, onChange, handleSubmit,
             <TitleInput name="title" placeholder={title} value={value} onChange={onChange} required/>
             <TextInput name="body" value={body} onChange={onChange} required />
             <Footer>
-                <div>
+                <Buttons>
                     <Button color="is-info" content={submitButtonName}/> 
                     <Button color="is-white" content="Cancel" onClick={handleCancel}/>
-                </div>
+                </Buttons>
             </Footer>
         </FormContainer>
     )

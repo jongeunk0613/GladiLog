420c634312f853f45854b70e1f675f321bd0f559
root <gladiolus4231@gmail.com>
2021-05-25 04:46:56 +0000

    [Feat] #12 : insert CommentList to CommentForm
    When a new comment is successfully created, add it to the comment array
    using the setComments function.
    Show the comments using the created component.

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import CommentForm from './CommentForm';
 import { useParams } from 'react-router-dom';
 
 import * as api from '../lib/api';
 import usePromise from '../hooks/usePromise';
+import CommentList from './CommentList';
 
 const Comment = () => {
     const { id } = useParams();
-    const [comments, setComments] = useState([]);
     const [body, setBody] = useState('');
+    const [loading, comments, error, setComments] = usePromise(() => api.getComments(id), []);
     
     const onChange = (event) => {
         setBody(event.target.value);
@@ -22,6 +23,7 @@ const Comment = () => {
             const response = await api.createComment(id, JSON.stringify({body}));
             if (response.status === 201) {
                 setBody('');
+                setComments([...comments, response.data.newComment])
             }
             
         } catch(e) {
@@ -31,7 +33,8 @@ const Comment = () => {
     
     return (
         <>
-            <CommentForm body={body} onChange={onChange} handleSubmit={handleSubmit}/>
+            <CommentForm total={comments? comments.length : 0} body={body} onChange={onChange} handleSubmit={handleSubmit}/>
+            <CommentList comments={comments}/>
         </>
     )
 }
